32e034265ec702acfa59436276744f8faa5c93f3
Nicci Sylvester <nes237@nau.edu>
2025-04-01 11:40:21 -0700

    android bridge

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
+// landing page of app
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
@@ -11,6 +12,7 @@ class HomePage extends StatelessWidget {
       color: Colors.white,
       child: InkWell(
         onTap: () {
+          // navigate to the WebViewPage when tapped
           Navigator.push(
             context,
             MaterialPageRoute(
@@ -50,6 +52,7 @@ class HomePage extends StatelessWidget {
   }
 }
 
+/// A stateful widget that displays a WebView
 class WebViewPage extends StatefulWidget {
   final String title;
 
@@ -67,30 +70,61 @@ class _WebViewPageState extends State<WebViewPage> {
   void initState() {
     super.initState();
 
+    // create parameters for the platform-specific WebView controller
     final PlatformWebViewControllerCreationParams params =
     PlatformWebViewControllerCreationParams();
 
+    // create the WebView controller from the platform-specific parameters
     controller = WebViewController.fromPlatformCreationParams(params)
-      ..setJavaScriptMode(JavaScriptMode.unrestricted);
-
-    // load the html from the assets folder
-    controller.loadFlutterAsset('assets/FlutterTest.html');
-
-    controller.setNavigationDelegate(
-      NavigationDelegate(
-        onPageFinished: (String url) {
-          setState(() {
-            isLoading = false;
-          });
+      // enable JavaScript execution
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      // set a navigation delegate to handle events like page load
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onPageFinished: (String url) {
+            setState(() {
+              isLoading = false;
+            });
+            // inject a JavaScript shim into the WebView
+            // this shim creates a global AndroidBridge object that the web content can use
+            controller.runJavaScript('''
+              window.AndroidBridge = {
+                // set player's name
+                setPlayerName: function(name) {
+                  // send a message to Flutter using the NativeBridge channel
+                  NativeBridge.postMessage(JSON.stringify({command: 'setPlayerName', value: name}));
+                },
+                // request location data
+                getLocationJSON: function() {
+                  // send a message to Flutter using the NativeBridge channel
+                  NativeBridge.postMessage(JSON.stringify({command: 'getLocationJSON'}));
+                }
+              };
+            ''');
+          },
+        ),
+      )
+    // register a JavaScript channel with the name 'NativeBridge'
+    // this channel receives messages from the web content
+      ..addJavaScriptChannel(
+        'NativeBridge',
+        onMessageReceived: (JavaScriptMessage message) {
+          // process messages from your HTML via the shim
+          // for example, handle "setPlayerName" or "getLocationJSON" commands.
+          print("Received from WebView: ${message.message}");
         },
-      ),
-    );
+      );
+
+    // TODO: load your HTML file from the assets folder
+    controller.loadFlutterAsset('assets/ScavengerHunt.html');
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(widget.title)),
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
       body: Stack(
         children: [
           WebViewWidget(controller: controller),
