b320d8e10cba7ed56da0acd8d784affdc4a8748c
cpendergraft98 <colepend98@gmail.com>
2025-05-08 01:12:34 -0700

    Clean Flutter metadata and regenerate from Linux for iOS build

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\Users\cpend\OneDrive\Documents\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\cpend\OneDrive\Documents\GitHub\NetGauge_Games"
+export "FLUTTER_ROOT=/home/psychedel0c/flutter"
+export "FLUTTER_APPLICATION_PATH=/home/psychedel0c/Documents/projects/NetGauge_Games"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
