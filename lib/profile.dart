a7bf3e6e4c445fa7517560594286d635ed87cbdb
Shelby Hagemann <52891544+shelbyhagemann@users.noreply.github.com>
2025-10-21 12:40:27 -0700

    Add logout function

--- lib/profile.dart
+++ lib/profile.dart
@@ -1,5 +1,7 @@
 // this will be where participants will have the option to logout
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'login_page.dart';
 
 
 class ProfilePage extends StatefulWidget {
@@ -11,27 +13,48 @@ class ProfilePage extends StatefulWidget {
 
 class _ProfilePageState extends State<ProfilePage> {
 
-  Future<void> _logout() async {
-
+  Future<void> logout() async {
+    try {
+      await FirebaseAuth.instance.signOut();
+      // Optional: Navigate to the login screen or a different screen after logout
+      Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => LoginPage()));
+    } on FirebaseAuthException catch (e) {
+      // Handle potential errors during sign out
+      print('Error signing out: $e');
+    }
   }
 
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('User Profile Page')),
+      appBar: AppBar(
+        title: const Text(
+          'User Profile',
+          style: TextStyle(
+            fontFamily: 'Roboto',
+            fontSize: 24,
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        backgroundColor: Theme.of(context).primaryColor,
+      ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: _logout,
-              child: const Text('Logout'),
-            ),
-          ],
-        ),
+        child:
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  logout();
+                },
+                child: const Text('Logout'),
+              ),
+            ],
+          ),
       ),
     );
   }
