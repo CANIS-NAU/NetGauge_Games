213351f3b142d0e4ce4a6d40e1c51f7f8dac32c9
Shelby Hagemann <52891544+shelbyhagemann@users.noreply.github.com>
2026-02-18 09:36:27 -0700

    Adding player statistics preview to homepage

--- lib/home.dart
+++ lib/home.dart
@@ -4,6 +4,7 @@ import 'speed_test_page.dart';
 import 'widgets/buttons.dart';
 import 'game_catalog.dart';
 import 'user_data_manager.dart';
+import 'package:provider/provider.dart';
 
 class Utilities {
   final String text;
@@ -34,115 +35,128 @@ class _HomePageState extends State<HomePage> {
   }
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          title: const Text('NetGauge Games'),
-          backgroundColor: Colors.deepPurple,
-          foregroundColor: Colors.white,
+    final userData = Provider.of<UserDataProvider>(context);
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text(
+            'NetGauge Games',
+            style: const TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+                fontSize: 25)
         ),
-        body: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text(
-                    'Welcome, user!',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    color: Colors.deepPurple,
-                    fontSize: 30)),
-                const SizedBox(height: 300),
-                // Utility buttons
-                GridView.builder(
-                  shrinkWrap: true,
-                  physics: const NeverScrollableScrollPhysics(),
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 4,
-                    crossAxisSpacing: 10,
-                    mainAxisSpacing: 10,
-                    childAspectRatio: 15 / 20,
+        backgroundColor: Colors.deepPurple,
+        foregroundColor: Colors.white,
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                width: double.infinity,
+                color: Colors.white,
+                child:
+                  Text(
+                      'Total Points Collected: ${userData.measurementsTaken} \n'
+                          'Total Distance Traveled: ${userData.distanceTraveled} \n'
+                          'Total Radius of Gyration: ${userData.totalRadiusGyration}',
+                      textAlign: TextAlign.start,
+                      style: const TextStyle(
+                          fontWeight: FontWeight.normal,
+                          color: Colors.deepPurple,
+                          fontSize: 20)
                   ),
-                  itemCount: utilityButtons.length,
-                  itemBuilder: (context, index) {
-                    return AppButtons(
-                      textColor: Colors.deepPurple,
-                      backgroundColor: Colors.white,
-                      borderColor: Colors.deepPurple,
-                      text: utilityButtons[index].text,
-                      icon: utilityButtons[index].icon,
-                      imagePath: utilityButtons[index].imagePath,
-                      isIcon: true,
-                      iconSize: 25,
-                      textSize: 12,
-                      buttonHeight: 60,
-                      buttonLength: 85,
-                      onTap: () async {
-                        String buttonText = utilityButtons[index].text;
-                        if (buttonText == 'Settings') {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => const SpeedTestPage(),
-                            ),
-                          );
-                        } else if (buttonText == 'Game Catalog') {
-                          await Navigator.push(
-                            context,
-                            MaterialPageRoute(builder: (context) => const GameCatalog()),
-                          );
-                          // 3. After returning, rebuild the UI to show the new favorites
-                          setState(() {});
-                        } else if (buttonText == 'Internet Measurment') {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => const SpeedTestPage(),
-                            ),
-                          );
-                        }
-                      },
-                    );
-                  },
+              ),
+              const SizedBox(height: 300),
+              // Utility buttons
+              GridView.builder(
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 4,
+                  crossAxisSpacing: 10,
+                  mainAxisSpacing: 10,
+                  childAspectRatio: 15 / 20,
                 ),
-                const SizedBox(height: 16),
-                // Game buttons
-                GridView.builder(
-                  shrinkWrap: true,
-                  physics: const NeverScrollableScrollPhysics(),
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2,
-                    crossAxisSpacing: 20,
-                    mainAxisSpacing: 20,
-                    childAspectRatio: 50 / 40,
-                  ),
-                  itemCount: favorite_games.length,
-                  itemBuilder: (context, index) {
-                    return AppButtons(
-                      textColor: Colors.black,
-                      backgroundColor: Colors.white,
-                      borderColor: Colors.black,
-                      text: favorite_games[index].text,
-                      icon: favorite_games[index].icon,
-                      imagePath: favorite_games[index].imagePath,
-                      isIcon: true,
-                      iconSize: 60,
-                      buttonHeight: 45,
-                      buttonLength: 45,
-                      onTap: () async {
-                        await showCustomPopup(context, favorite_games[index]);
+                itemCount: utilityButtons.length,
+                itemBuilder: (context, index) {
+                  return AppButtons(
+                    textColor: Colors.deepPurple,
+                    backgroundColor: Colors.white,
+                    borderColor: Colors.deepPurple,
+                    text: utilityButtons[index].text,
+                    icon: utilityButtons[index].icon,
+                    imagePath: utilityButtons[index].imagePath,
+                    isIcon: true,
+                    iconSize: 25,
+                    textSize: 12,
+                    buttonHeight: 60,
+                    buttonLength: 85,
+                    onTap: () async {
+                      String buttonText = utilityButtons[index].text;
+                      if (buttonText == 'Settings') {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const SpeedTestPage(),
+                          ),
+                        );
+                      } else if (buttonText == 'Game Catalog') {
+                        await Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => const GameCatalog()),
+                        );
+                        // 3. After returning, rebuild the UI to show the new favorites
                         setState(() {});
-                      },
-                    );
-                  },
+                      } else if (buttonText == 'Internet Measurment') {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const SpeedTestPage(),
+                          ),
+                        );
+                      }
+                    },
+                  );
+                },
+              ),
+              const SizedBox(height: 16),
+              // Game buttons
+              GridView.builder(
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 20,
+                  mainAxisSpacing: 20,
+                  childAspectRatio: 50 / 40,
                 ),
-              ]
-            ),
-        ),
-        ),
+                itemCount: favorite_games.length,
+                itemBuilder: (context, index) {
+                  return AppButtons(
+                    textColor: Colors.black,
+                    backgroundColor: Colors.white,
+                    borderColor: Colors.black,
+                    text: favorite_games[index].text,
+                    icon: favorite_games[index].icon,
+                    imagePath: favorite_games[index].imagePath,
+                    isIcon: true,
+                    iconSize: 60,
+                    buttonHeight: 45,
+                    buttonLength: 45,
+                    onTap: () async {
+                      await showCustomPopup(context, favorite_games[index]);
+                      setState(() {});
+                    },
+                  );
+                },
+              ),
+            ]
+          ),
+      ),
       ),
     );
   }
--- lib/user_data_manager.dart
+++ lib/user_data_manager.dart
@@ -24,6 +24,7 @@ class UserDataProvider extends ChangeNotifier {
   String get uid => _userData?['uid'] ?? '';
   String get email => _userData?['email'] ?? '';
   int get distanceTraveled => _userData?['distanceTraveled'] ?? 0;
+  int get totalRadiusGyration => _userData?['totalRadiusGyration'] ?? 0;
   List<dynamic> get dataPoints => _userData?['dataPoints'] ?? [];
 
   // Fetch data for the currently logged-in user
